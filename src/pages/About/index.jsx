70c200da00217ba7833f144bffff817317c45db2
Marie Rodiet <marie.rodiet@gmail.com>
2022-05-10 10:22:55 +0200

    Add Banner component and Routes

---
+++ src/pages/About/index.jsx
@@ -0,0 +1,10 @@
+import Banner from './../../components/Banner';
+
+function About() {
+    return (
+        <div>
+            <h1>Page A Propos!!!!!</h1>
+        </div>
+    );
+}
+export default About;
